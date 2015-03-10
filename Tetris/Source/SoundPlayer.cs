ae681ada0126557b6b6fb612b3caca183ade768c
vital01 <vital_yeutukhovich@epam.com>
2015-03-10 15:52:52 +0300

    Убрал ограничение на музыкальный фон
    Сделал так, чтобы подтягивало всю музыку из папки Music с рсширением
    .ogg. Отделяет Menu, остальное кидает в плейлист

--- Tetris/Source/SoundPlayer.cs
+++ Tetris/Source/SoundPlayer.cs
@@ -8,38 +8,35 @@ namespace Tetris
 {
     public static class SoundPlayer
     {
+        private const string MenuPath = "Music\\Menu.ogg";
+
         public enum Sounds { Hit, Line, Pause, Switch, Select_0, Select_1, Error, GameOver }
-        private enum Music { Menu, Background_0, Background_1, Background_2 }
         private static Audio Menu { get; set; }
-        private static List<Audio> PlayList { get; set; }
+        private static List<Audio> GamePlaylist { get; set; }
         //звук,соответствующий энаму
         private static Dictionary<Sounds, Audio> SoundsDictionary { get; set; }
         private static int CurrentTrack { get; set; }
 
         public static void LoadMusic()
         {
-            for (Music music = 0; (int)music < 4; music++)
+            foreach (string music in Directory.GetFiles(@"Music", "*.ogg"))
             {
-                string path = @"Music\" + music.ToString() + ".ogg";
-                if (File.Exists(path))
+                if (File.Exists(music))
                 {
-                    if (music == Music.Menu)
+                    if (music.Equals(MenuPath))
                     {
-                        Menu = new Audio(path);
+                        Menu = new Audio(music);
                         Menu.Ending += new EventHandler((sender, e) => { Menu.CurrentPosition = 0; });
                     }
                     else
                     {
-                        if (music == Music.Background_0 || music == Music.Background_1 || music == Music.Background_2)
+                        if (GamePlaylist == null)
                         {
-                            if (PlayList == null)
-                            {
-                                PlayList = new List<Audio>();
-                            }
-                            PlayList.Add(new Audio(path));
-                            //вешаем событие на переключение по плейлисту
-                            PlayList[PlayList.Count - 1].Ending += new EventHandler((sender, e) => { CurrentTrack = (CurrentTrack + 1) % PlayList.Count; PlayList[CurrentTrack].CurrentPosition = 0; PlayList[CurrentTrack].Play(); });
+                            GamePlaylist = new List<Audio>();
                         }
+                        GamePlaylist.Add(new Audio(music));
+                        //Событие на переключение на следующий трек плейлиста по завершении предыдущего
+                        GamePlaylist[GamePlaylist.Count - 1].Ending += new EventHandler((sender, e) => { CurrentTrack = (CurrentTrack + 1) % GamePlaylist.Count; GamePlaylist[CurrentTrack].CurrentPosition = 0; GamePlaylist[CurrentTrack].Play(); });
                     }
                 }
             }
@@ -67,11 +64,11 @@ namespace Tetris
             {
                 SoundsDictionary[sound].CurrentPosition = 0;
                 SoundsDictionary[sound].Play();
-            }           
+            }
         }
 
         public static void PlayMenu()
-        {           
+        {
             if (Menu != null)
             {
                 Menu.Play();
@@ -88,40 +85,40 @@ namespace Tetris
 
         public static void PlayBackground()
         {
-            if (PlayList != null)
+            if (GamePlaylist != null)
             {
-                PlayList[CurrentTrack].Play();
+                GamePlaylist[CurrentTrack].Play();
             }
         }
 
         public static void StopBackground()
         {
-            if (PlayList != null)
+            if (GamePlaylist != null)
             {
-                PlayList[CurrentTrack].Stop();
+                GamePlaylist[CurrentTrack].Stop();
                 CurrentTrack = 0;
             }
         }
 
         public static void PauseBackground()
         {
-            if (PlayList != null)
+            if (GamePlaylist != null)
             {
-                PlayList[CurrentTrack].Pause();
+                GamePlaylist[CurrentTrack].Pause();
             }
         }
 
         public static void ResumeBackground()
         {
-            if (PlayList != null)
+            if (GamePlaylist != null)
             {
-                PlayList[CurrentTrack].Play();
+                GamePlaylist[CurrentTrack].Play();
             }
-        } 
+        }
 
         public static bool SoundFinished(Sounds sound)
         {
             return SoundsDictionary.ContainsKey(sound) && !(SoundsDictionary[sound].CurrentPosition == SoundsDictionary[sound].Duration);
-        }               
+        }
     }
 }
